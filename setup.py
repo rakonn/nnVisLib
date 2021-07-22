a3a5cfd0c8d5489820daa357eea23aee9bcb847b
rakonn <christiantr55@gmail.com>
2021-07-22 22:43:43 +0200

    project structure added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from os import name
+from setuptools import find_packages, setup, version
+
+setup(
+    name="",
+    packages=find_packages(include=["..."]),
+    version="0.1.0",
+    description="",
+    author="",
+    license="MIT",
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+    test_suite="",
+)
